109e751e75174785d406e6e6fa9a06afec99a245
Minh Hieu <95726917+Zeustakeshi@users.noreply.github.com>
2023-05-13 13:17:29 +0700

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ connectDB();
 
 // middleware
 app.use(cookieParser());
-// app.use(helmet());
+app.use(helmet());
 app.use(cors({ credentials: true, origin: process.env.ORIGIN_URL }));
 // app.use(cors());
 app.use(express.json());
